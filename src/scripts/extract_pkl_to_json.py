83589b5fa67b3d11ab46e4bf6049c0169a577d45
João Bosco Martinolli <joaoboscojbm@gmail.com>
2024-11-28 18:33:04 +0400

    Aviation Corpus Json file from pkl file

---
+++ src/scripts/extract_pkl_to_json.py
@@ -0,0 +1,18 @@
+import pickle
+import json
+import os
+
+# Define paths
+
+pkl_path = os.path.join('data', 'raw', 'aviation_corpus.pkl')
+json_path = os.path.join('data', 'processed','aviation_corpus.json')
+
+# Load the pickle file
+with open(pkl_path, 'rb') as file:
+    corpus = pickle.load(file)
+
+# Convert the pickle file to a JSON format
+with open(json_path, 'w', encoding='utf-8') as json_file:
+    json.dump(corpus, json_file, ensure_ascii=False, indent=4)
+
+print(f"Data successfully extracted and saved to {json_path}")
