b75f74ff9e8c67029afdb55cdccf1c7249707f45
João Bosco Martinolli <joaoboscojbm@gmail.com>
2024-12-11 18:00:03 +0400

    Change bat file

--- update_data.bat
+++ update_data.bat
@@ -1,6 +1,4 @@
 @echo off
-dvc add data/raw/aviation_corpus.pkl
-git add data/raw/aviation_corpus.pkl.dvc
 dvc add data/processed/aviation_corpus.json
 git add data/processed/aviation_corpus.json.dvc
 git commit -m "Update aviation data"
