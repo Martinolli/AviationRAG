52dd85bb39bfe81d40dae051d95b20c6ee3e6b04
João Bosco Martinolli <joaoboscojbm@gmail.com>
2025-01-02 23:36:05 +0400

    Update files to run vercel

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to AviationRAG</h1>
+            <p>Your application is running successfully!</p>
+        </div>
+    );
+}
