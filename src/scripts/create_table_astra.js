a6667512b87f542d284f28bee25f55a1f4c785c7
João Bosco Martinolli <joaoboscojbm@gmail.com>
2025-02-07 20:26:06 +0400

    added new documents

--- src/scripts/create_table_astra.js
+++ src/scripts/create_table_astra.js
@@ -1,7 +1,11 @@
-const cassandra = require('cassandra-driver');
-const dotenv = require('dotenv');
+import cassandra from 'cassandra-driver';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 async function createTable() {
     const client = new cassandra.Client({
